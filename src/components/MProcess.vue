90e649bff3c4689924fc38f5af31071e7bc66a3c
刘顺 <909275705@qq.com>
2022-10-18 21:45:31 +0800

    修复 进程table横向无限拉伸问题

---
+++ src/components/MProcess.vue
@@ -0,0 +1,26 @@
+<template>
+  <m-card class="box-card" title="进程">
+    <!--为解决table 横向无限拉伸问题-->
+    <div style="display: grid">
+      <el-table :data="[]" border>
+        <!--      <el-table-column prop="date" label="Date" width="180" />-->
+        <!--      <el-table-column prop="name" label="Name" width="180" />-->
+        <!--      <el-table-column prop="address" label="Address" />-->
+      </el-table>
+    </div>
+  </m-card>
+</template>
+
+<script setup lang="ts">
+
+  import {Process} from "~/ws_protocol";
+
+  const props = defineProps<{
+    process: [Process]
+  }>();
+
+</script>
+
+<style scoped>
+
+</style>

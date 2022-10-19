c1c9ed9f827e503649f221cd55a217de27227cbb
刘顺 <909275705@qq.com>
2022-10-20 00:52:40 +0800

    完善 进程组件添加详情按钮

--- src/components/MProcess.vue
+++ src/components/MProcess.vue
@@ -2,14 +2,26 @@
   <m-card class="box-card" title="进程">
     <!--为解决table 横向无限拉伸问题-->
     <div style="display: grid">
-      <el-table :data="props.process" border>
+      <el-table :data="props.process">
         <el-table-column prop="pid" label="进程id" />
         <el-table-column prop="name" label="名称" sortable />
-        <el-table-column prop="cpu_usage" label="Cpu使用" />
-        <el-table-column prop="memory" label="内存使用" />
+        <el-table-column prop="cpu_usage" label="Cpu使用">
+          <template #default="scope">
+            <span>{{ scope.row.cpu_usage.toFixed(1) + ' %' }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="memory" label="内存使用" >
+          <template #default="scope">
+            <span>{{ $filters.formatBytes(scope.row.memory) }}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="parent" label="父进程" />
-
-        <!--      <el-table-column prop="address" label="Address" />-->
+        <el-table-column label="操作" width="120">
+          <template #default>
+            <el-button link type="primary" size="small" @click="showDetail"
+            >详情</el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </div>
   </m-card>
@@ -22,8 +34,10 @@
   const props = defineProps<{
     process: [Process]
   }>();
-  console.log(props.process);
+  props.process.map(item => item.status).forEach(console.log)
+  const showDetail = () => {
 
+  }
 </script>
 
 <style scoped>

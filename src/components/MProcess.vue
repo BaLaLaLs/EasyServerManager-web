1af2a4e8fb2d009c3e86109ece1566781601d712
刘顺 <909275705@qq.com>
2022-10-20 00:41:03 +0800

    完善 进程组件

--- src/components/MProcess.vue
+++ src/components/MProcess.vue
@@ -2,9 +2,13 @@
   <m-card class="box-card" title="进程">
     <!--为解决table 横向无限拉伸问题-->
     <div style="display: grid">
-      <el-table :data="[]" border>
-        <!--      <el-table-column prop="date" label="Date" width="180" />-->
-        <!--      <el-table-column prop="name" label="Name" width="180" />-->
+      <el-table :data="props.process" border>
+        <el-table-column prop="pid" label="进程id" />
+        <el-table-column prop="name" label="名称" sortable />
+        <el-table-column prop="cpu_usage" label="Cpu使用" />
+        <el-table-column prop="memory" label="内存使用" />
+        <el-table-column prop="parent" label="父进程" />
+
         <!--      <el-table-column prop="address" label="Address" />-->
       </el-table>
     </div>
@@ -18,6 +22,7 @@
   const props = defineProps<{
     process: [Process]
   }>();
+  console.log(props.process);
 
 </script>
 
